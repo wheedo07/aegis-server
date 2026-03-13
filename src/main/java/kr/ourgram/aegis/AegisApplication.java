de47e6c4cbd864adca78cefe6e0ea1127df99840
brillianintdoh <wheedo07@ourgram.co.kr>
2026-03-13 13:31:43 +0900

    first commit

---
+++ src/main/java/kr/ourgram/aegis/AegisApplication.java
@@ -0,0 +1,13 @@
+package kr.ourgram.aegis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AegisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ourgram/aegis/AegisApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ourgram.aegis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
