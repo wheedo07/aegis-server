0e04c58748c25135f4ffc27b2b5a1b7d636cc290
brillianintdoh <wheedo07@ourgram.co.kr>
2026-03-15 13:57:17 +0900

    build 스크립트 작성 및 프로젝트 제작 준비

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+dir=$(basename "$PWD")
+if [[ "$dir" != "server" && "$dir" !=  "aegis-server" ]]; then
+    echo "프로젝트 ROOT 경로에서 실행해주세요"
+    exit 1
+fi
+
+webpack && mvn clean install;
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+cp target/aegis-*.jar build
